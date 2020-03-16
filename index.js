e8bddf8fa68c97b02fa2e675910c36379f57f4dc
ajlanza <61472953+ajlanza@users.noreply.github.com>
2020-03-16 09:54:55 -0400

    check button works
    added check button capability, cleaned up adding li

--- index.js
+++ index.js
@@ -4,21 +4,29 @@ $(function() {
       event.preventDefault();
       const newItem = $(this).find('#shopping-list-entry');
       $("ul").append(
-        '<li>' +
-        '<span class="shopping-item">' + `${newItem.val()}` + '</span>' +
-        '<div class="shopping-item-controls">' +
-          '<button class="shopping-item-toggle">' +
-            '<span class="button-label">check</span>' +
-          '</button>' +
-          '<button class="shopping-item-delete">' +
-            '<span class="button-label">delete</span>' +
-          '</button>' +
-        '</div>' +
-      '</li>'
+        `<li>
+        <span class="shopping-item"> ${newItem.val()}</span>
+        <div class="shopping-item-controls">
+          <button class="shopping-item-toggle">
+            <span class="button-label">check</span>
+          </button>
+          <button class="shopping-item-delete">
+            <span class="button-label">delete</span>
+          </button>
+        </div>
+      </li>`
       );
     }); 
-    //remove the entire li if delete is clicked
+
+    //remove the entire li element when delete button is clicked
     $('ul').on('click', '.shopping-item-delete', function(event) {
         $(this).closest('li').remove();
       });
+
+    //toggle if an item is checked
+    $('ul').on('click', '.shopping-item-toggle', function(event) {
+        $(this).closest('li').children('.shopping-item').toggleClass('shopping-item__checked');
+      });
+
+
   })
